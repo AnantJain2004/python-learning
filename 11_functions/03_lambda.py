582b17121f7268a64248113454998a7bb2f80387
Anant Jain <ajanantzen2004@gmail.com>
2025-04-18 15:16:36 +0530

    Add lambda functions

---
+++ 11_functions/03_lambda.py
@@ -0,0 +1,9 @@
+# lambda functions -> are anonymous, one-line functions (no need for def and return keywords)
+
+# lambda function to add two numbers
+add = lambda a, b: a+b
+print(f"Addition: {add(5,4)}")
+
+# lamda function to sqaure a number
+square = lambda x: x*x
+print(f"Square: {square(5)}")
