7931cd34b4206958f4e765f712c1ffc73232768a
Anant Jain <ajanantzen2004@gmail.com>
2025-04-20 00:22:29 +0530

    Add examples for local and global variables

--- 11_functions/06_function_scope.py
+++ 11_functions/06_function_scope.py
@@ -1,7 +1,15 @@
 # Scope - where a variable can be accessed 
-def sum(a,b):
+def sum(a,b): # a and b are local variables
     c = a+b
+    z = 1 # It creates a local variable called z which is destroyed when this function returns
     return c
 
+def greet():
+    z = 46 # Local variable
+    print("Hello!")
+
+z = 8 # z is a global variable
+print(f"Value of z before calling the function: {z}")
 print(sum(5,6))
+print(f"Value of z after calling the function: {z}")
 # print(c) # Error : c is not defined outside the function
