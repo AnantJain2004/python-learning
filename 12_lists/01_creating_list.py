24454557066a644f17a6fef3f803b0dccf804e50
Anant Jain <ajanantzen2004@gmail.com>
2025-04-20 23:54:23 +0530

    Add List

---
+++ 12_lists/01_creating_list.py
@@ -0,0 +1,7 @@
+# List -> ordered, mutable collection of items, enclosed in square brackets[]
+
+numbers = [10, 20, 30, 40]
+print(numbers)
+
+mixed = [46, "Jack", False, 3.4, [1, 2, 3]]
+print(mixed)
---
+++ 12_lists/02_accessing_elements.py
@@ -0,0 +1,16 @@
+numbers = [10, 20, 30, 40, 50, 60]
+
+print("\nAccessing by index")
+# Accessing by index
+print(numbers[0])
+print(numbers[2])
+print(numbers[5])
+
+print("\nLooping through list")
+# Looping through list
+for num in numbers:
+    print(num)
+
+print("\nSlicing list")
+# Slicing List
+print(numbers[1:4])
