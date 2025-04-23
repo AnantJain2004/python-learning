2e9a925cf50ca766d376575ef5764368b4f64793
Anant Jain <ajanantzen2004@gmail.com>
2025-04-24 02:31:17 +0530

    Add list comprehension

---
+++ 12_lists/04_list_comprehension.py
@@ -0,0 +1,11 @@
+# Create a list containing the table of 5
+table = []
+
+for i in range(1,11): # this for loop will populate the list which was initially empty
+    table.append(5*i)
+
+print(table)
+
+# We can this 5 lines of code in a single line using List Comprehension
+table = [5*i for i in range(1,11)]  # List Comprehension
+print(table)
