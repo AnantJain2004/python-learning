2feda8061b95a02cf0609c73e68d6b2f015b1673
Anant Jain <ajanantzen2004@gmail.com>
2025-05-05 21:19:27 +0530

    Add Instance, Class and Static Methods

--- 17_python_advanced_concepts/04_static_class_instance_methods.py
+++ 17_python_advanced_concepts/04_static_class_instance_methods.py
@@ -8,15 +8,26 @@ class Employee:
         self.name = name
         self.salary = salary
 
-    # Instance method - default methods that we create within a class
+    # Instance method - work on objects(instances) of a class
     def print_info(self):
         info = f"The name is {self.name} and the salary is {self.salary}."
         print(info)
 
-    # Static Method
+    # Static Method - ● a helper method, that does not need access to object(self) or class(cls)
+    #                 ● used when a method relates to the class logically but doesn't need its data
     @staticmethod
     def sum(a ,b):
         return a+b
+    
+    # Class Method - works on the class itself, not on objects
+    @classmethod
+    def print_company(cls):
+        print(cls.company)
+
+    # to change company name
+    @classmethod
+    def change_company(cls, new_company):
+        cls.company = new_company
 
 e1 = Employee("Jack", 35000)
 e2 = Employee("Alan", 47000)
@@ -26,4 +37,12 @@ print(Employee.company)
 e1.print_info()
 e2.print_info()
 
-print(e2.sum(32, 78))
+print(Employee.sum(32, 78))
+
+# Employee.print_company()
+# Employee.change_company("DELL")
+# Employee.print_company()
+
+print(Employee.company)
+Employee.change_company("DELL")
+print(Employee.company)
