e3023896d555c1e9abaf479c0a261220c9dd6f9d
Anant Jain <ajanantzen2004@gmail.com>
2025-05-26 23:53:31 +0530

    Add Walrus Operator

---
+++ 17_python_advanced_concepts/13_walrus.py
@@ -0,0 +1,6 @@
+# walrus operator: the walrus operator(:=) is used to assign a value to a variable inside an expression
+# It allows you to:
+#   ∘ Assign and use a variable in a single expression
+#   ∘ Make your code shorter and more readable
+
+# Assign inside a condition
