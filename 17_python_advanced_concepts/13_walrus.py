1938b707b04c89568d9c2e7c01442158befe3b76
Anant Jain <ajanantzen2004@gmail.com>
2025-05-27 00:09:36 +0530

    Update Walrus Operator

--- 17_python_advanced_concepts/13_walrus.py
+++ 17_python_advanced_concepts/13_walrus.py
@@ -3,4 +3,24 @@
 #   ∘ Assign and use a variable in a single expression
 #   ∘ Make your code shorter and more readable
 
+
 # Assign inside a condition
+
+# (Tradiitonal approach)
+# length = len("Python")
+# if(length > 5):
+#     print(f"Length is {length}")
+
+# (with walrus)
+if (length := len("Python")) > 5:
+    print(f"Length is {length}")
+
+# Using in a while loop
+# (traditional approach)
+# data = input("Enter text: ")
+# while(data != "exit"):
+#     print("You typed: {data}")
+
+# (with walrus)
+while (data := input("Enter text: ")) != "exit":
+    print(f"You typed: {data}")
