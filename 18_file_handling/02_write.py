c92020679ff79f7155476e45ec70592632fcc4db
Anant Jain <ajanantzen2004@gmail.com>
2025-05-29 02:13:55 +0530

    File Handling - write to a file

---
+++ 18_file_handling/02_write.py
@@ -0,0 +1,10 @@
+f = open("write into this.txt", "w")
+
+content = '''
+Hey there, this is Anant and I love writing code in Python.
+I work with NumPy and Pandas
+'''
+
+f.write(content)
+
+f.close()
