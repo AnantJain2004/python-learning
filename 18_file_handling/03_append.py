c44a6feccbb9211d18bc28f62555aeb803a04a00
Anant Jain <ajanantzen2004@gmail.com>
2025-05-29 16:29:51 +0530

    Update File Handling -  Append to a file

--- 18_file_handling/03_append.py
+++ 18_file_handling/03_append.py
@@ -1,3 +1,5 @@
+# 'a' (Append mode) - If the file exists, the data will be added to the end of the file. If the file doesn't exist, a new file will be created 
+
 f = open("write into this.txt", "a")
 
 content = '''
