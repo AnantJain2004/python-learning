9c552baa201e992f4e0cfc8d4cae635e5ffc4cf9
Anant Jain <ajanantzen2004@gmail.com>
2025-06-02 04:06:27 +0530

    Working with Virtual Environments

---
+++ 19_external_modules/01_venv.py
@@ -0,0 +1,6 @@
+# >> "pip install virtualenv" - Download virtual environment package
+# >> "python -m venv env1" - to create a virtual env. named env1
+# >> ".\env1\Scripts\Activate.ps1" - to activate virtual environment
+# >> "pip list" - to list all packages
+# >> "pip freeaze > requirements.txt" - to write all packages inside requirements.txt file
+# >> "deactivate" - to deactivate virtual env. 
